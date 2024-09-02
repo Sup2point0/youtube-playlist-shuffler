98d5f496829b2675f08c23044fac64794ea7fa5f
Sup#2.0 <sup2point0@gmail.com>
2024-09-02 18:50:23 +0100

    implement script

--- shuffle/__main__.py
+++ shuffle/__main__.py
@@ -1,17 +1,32 @@
+print(">> running!")
+
 import json
 
 from config import ROOT, SCRIPT
 from auth import authorise
+from playlist import Playlist
 
 
 yt = authorise()
+print(" / authorised!")
 
 with open(SCRIPT / "playlists.json") as source:
   playlists = json.load(source)
 
-request = yt.playlistItems().list(
-  part = "snippet",
-  playlistId = playlists["Maximum Rhythm"],
-)
-response = request.execute()
-print(response)
+
+p = Playlist(yt, playlists["Maximum Rhythm"])
+p.fetch()
+p.save(ROOT / "data" / "Maximum Rhythm.json")
+
+print("BEFORE =", len(p.videos))
+for video in p.videos[:9]:
+  print(video['snippet']['title'])
+
+p.shuffle(freeze_start = 3)
+
+print("AFTER =", len(p.videos))
+for video in p.videos[:9]:
+  print(video['snippet']['title'])
+p.update()
+
+print(">> done!")
